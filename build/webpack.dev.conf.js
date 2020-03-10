59779a7e7a08c1cab849c64f3076b7c5e2139810
IsmagilArslanov <ins-arslanov@mail.ru>
2020-03-10 12:51:23 +0400

    changed the publicPath to ""

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -4,6 +4,7 @@ const baseWebpackConfig = require('./webpack.base.conf')
 
 const devWebpackConfig = merge(baseWebpackConfig, {
   // DEV config
+  
   mode: 'development',
   devtool: 'cheap-module-eval-source-map',
   devServer: {
