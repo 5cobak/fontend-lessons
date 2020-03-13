b8eacc3c80c77f9220e2442e8f66353ddca98b89
IsmagilArslanov <ins-arslanov@mail.ru>
2020-03-13 17:51:00 +0400

    added item-quantity-dropdown, added date-picker

--- src/js/index.js
+++ src/js/index.js
@@ -3,40 +3,57 @@
 
 // item-quantity-dropdown js
 
-// $(document).ready(() => {
-//   $('.iqdropdown').iqDropdown({
-//   // max total items
-//   maxItems: Infinity,
-//   // min total items
-//   minItems: 3,
-//   // text to show on the dropdown override data-selection-text attribute
-//   selectionText: 'item',
-//   // text to show for multiple items
-//   textPlural: 'items',
-//   // optionally can use setSelectionText function to override selectionText
-//   setSelectionText: (itemCount, totalItems) => { /* return string */ },
-//   // buttons to increment/decrement
-//   controls: {
-//     position: 'right',
-//     displayCls: 'iqdropdown-item-display',
-//     controlsCls: 'iqdropdown-item-controls',
-//     counterCls: 'counter'
-//   },
-//   // fires when an item quantity changes
-//   onChange: (id, count, totalItems) => {},
-//   // return false to prevent an item decrement
-//   beforeDecrement: (id, itemCount) => {},
-//   // return false to prevent an item increment
-//   beforeIncrement: (id, itemCount) => {}
+$(document).ready(() => {
+  $('.iqdropdown').iqDropdown({
+  // max total items
+  // maxItems: Infinity,
+  // min total items
+  minItems: "",
+  // text to show on the dropdown override data-selection-text attribute
+  selectionText: 'гость',
+  // text to show for multiple items
+  textPlural: 'гостей',
+  // optionally can use setSelectionText function to override selectionText
+  setSelectionText: (itemCount, totalItems) => {
+  	// if(totalItems == 0) return "Сколько гостей";
+  	 // line declination function
+				function declination(n, text_forms){
 
-//   });
-// });
+			    n = Math.abs(n) % 100; var n1 = n % 10;
+			    if (n >= 5 && n < 20) { return text_forms[2]; }
+			    if (n1 > 1 && n1 <= 4) { return text_forms[1]; }
+			    if (n1 == 1) { return text_forms[0]; }
+			    if(n == 0) {return text_forms[3]}
+
+			    return text_forms[2];
+
+			};
+			if (totalItems == 0) return declination(totalItems, ["гость", "гостя", "гостей", "Сколько гостей"]);
+			else return totalItems + " " +declination(totalItems, ["гость", "гостя", "гостей", "Сколько гостей"]);
+  },
+  // buttons to increment/decrement
+
+  // fires when an item quantity changes
+  onChange: (id, count, totalItems) => {
+       
+    },
+  // return false to prevent an item decrement
+  // return false to prevent an item increment
+  });
+
+
+
+});
 
 // mask 
 
 $(document).ready(function(){
-  $('.date-field').mask('00.00.0000');
+  $('.date-field').mask('00.00.0000')
 });
+
+// air-datePicker 
+
+$('.my-datepicker').datepicker()
 
 //svg-sprite-loader
 
@@ -54,9 +71,8 @@ fetch(`https://mycdn.com/img/icons.svg`).then(res => {
 });
 
 
-// air-datePicker 
+//borderBottomFocus on dropdown elements 
 
-$('.datepicker-here').datepicker([options]);
-
-// Доступ к экземпляру объекта
-$('.datepicker-here').data('datepicker');
+$('.iqdropdown').click(function(){
+	$(this).addClass('borderBottomFocus');
+}) 
