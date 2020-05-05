664b1f70f44352dcb56c27f9220ae71106c55751
IsmagilArslanov <ins-arslanov@mail.ru>
2020-05-05 16:02:39 +0400

    add box-shadow for slide

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -4,16 +4,40 @@ import Swiper from 'swiper';
 jQueryBridget('swiper', Swiper, $);
 
 $(document).ready(() => {
-  $('.swiper-container').swiper({
-    init: true,
-    speed: 400,
-    spaceBetween: 0,
-    effect: 'slide',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-    },
-    initialSlide: 0,
-    resistanceRatio: 0,
+  // $('.swiper-container').swiper({
+  //   init: true,
+  //   speed: 400,
+  //   spaceBetween: 0,
+  //   effect: 'slide',
+  //   pagination: {
+  //     el: '.swiper-pagination',
+  //     type: 'bullets',
+  //   },
+  //   initialSlide: 0,
+  //   resistanceRatio: 0,
+  //   navigation: {
+  //     nextEl: '.swiper-button-next',
+  //     prevEl: '.swiper-button-prev',
+  //   },
+  // });
+  // eslint-disable-next-line
+  $('.swiper-container').each(function(){
+    $(this).swiper({
+      el: $(this),
+      init: true,
+      speed: 400,
+      spaceBetween: 0,
+      effect: 'slide',
+      pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+      },
+      initialSlide: 0,
+      resistanceRatio: 0,
+      navigation: {
+        nextEl: $(this).find('.swiper-button-next'),
+        prevEl: $(this).find('.swiper-button-prev'),
+      },
+    });
   });
 });
