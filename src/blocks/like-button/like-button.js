85ead65c0001dc31d7a879d1b2ad5b9f0f055c35
5cobak <55190870+5cobak@users.noreply.github.com>
2021-01-02 17:29:18 +0400

    replace bissnes logic into classes

--- src/blocks/like-button/like-button.js
+++ src/blocks/like-button/like-button.js
@@ -1,16 +1,39 @@
+/* eslint-disable no-unused-vars */
 // eslint-disable-next-line
-$('.like-button').on('click', function () {
-  let countLike = +$(this).find('.js-like-button__likes').text();
-  if (!$(this).hasClass('like-button_active')) {
-    $(this).find('.js-like-button__like').css('opacity', '0');
-    $(this).find('.js-like-button__like-favorite').css('opacity', '1');
-    countLike += 1;
-    $(this).find('.js-like-button__likes').text(countLike);
-  } else {
-    $(this).find('.js-like-button__like').css('opacity', '1');
-    $(this).find('.js-like-button__like-favorite').css('opacity', '0');
-    countLike -= 1;
-    $(this).find('.js-like-button__likes').text(countLike);
+$(document).ready(() => {
+  class LikeBbutton {
+    constructor($elem) {
+      this.$elem = $elem;
+      this.countLike = +$elem.find('.js-like-button__likes').text();
+
+      this.init();
+    }
+
+    init() {
+      const $elem = this.$elem;
+      let count = this.countLike;
+
+      function toggleLike() {
+        const countLike = +$elem.find('.js-like-button__likes').text();
+        if (!$elem.hasClass('like-button_active')) {
+          $elem.find('.js-like-button__like').css('opacity', '0');
+          $elem.find('.js-like-button__like-favorite').css('opacity', '1');
+          count += 1;
+          $elem.find('.js-like-button__likes').text(count);
+        } else {
+          $elem.find('.js-like-button__like').css('opacity', '1');
+          $elem.find('.js-like-button__like-favorite').css('opacity', '0');
+          count -= 1;
+          $elem.find('.js-like-button__likes').text(count);
+        }
+        $elem.toggleClass('like-button_active');
+      }
+
+      $elem.on('click', toggleLike);
+    }
   }
-  $(this).toggleClass('like-button_active');
+
+  const likeButtons = document.querySelectorAll('.js-like-button');
+
+  likeButtons.forEach((item) => new LikeBbutton($(item)));
 });
