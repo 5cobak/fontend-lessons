083424021f10252c3fd131a2f4f7d94ce2d4b0ab
5cobak <55190870+5cobak@users.noreply.github.com>
2021-02-02 16:47:59 +0400

    fixed gamburger #18

--- src/components/header/Burger.js
+++ src/components/header/Burger.js
@@ -7,11 +7,12 @@ export default class Burger {
   init() {
     this.parents.forEach((header) => {
       const burger = header.querySelector('.js-header__burger');
+      const layout = header.querySelector('.js-header__burger-layout');
       const headerDropdown = header.querySelector('.js-header__nav_dropdown');
       const shadow = header.querySelector('.js-header__nav-shadow');
 
       function hideShowMenu() {
-        $(burger).toggleClass('header__burger_active');
+        $(layout).toggleClass('header__burger-layout_active');
         $(headerDropdown).toggleClass('header__nav_dropdown-active');
         $(shadow).toggleClass('header__nav-shadow_active');
         $('body').toggleClass('stop-scrolling');
