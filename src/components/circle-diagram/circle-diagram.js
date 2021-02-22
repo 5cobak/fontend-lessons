e3ec4766ee7fc1465c8fd1ad821e3bd6912798b9
5cobak <55190870+5cobak@users.noreply.github.com>
2021-02-22 15:00:02 +0400

    fixed call into circle-diagram

--- src/components/circle-diagram/circle-diagram.js
+++ src/components/circle-diagram/circle-diagram.js
@@ -2,6 +2,6 @@ import CircleDiagram from './CircleDiagram';
 
 $(document).ready(() => {
   const el = document.querySelector('.circle-diagram');
-
+  if (!el) return;
   new CircleDiagram(el);
 });
