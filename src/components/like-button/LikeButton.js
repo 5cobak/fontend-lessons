bdd9e47b4f5cbb9f550f3a8e8f7e6d4be71c3b72
5cobak <ins-arslanov@mail.ru>
2021-06-20 10:56:23 +0300

    fixed overflow at header/fixed like-button/fixed logo/fixed white button overflow, changed button at datepicker and dropdown

--- src/components/like-button/LikeButton.js
+++ src/components/like-button/LikeButton.js
@@ -8,13 +8,9 @@ class LikeButton {
   _toggleLike() {
     const { $button } = this;
     if (!$button.hasClass('like-button_active')) {
-      $button.find('.js-like-button__like').css('opacity', '0');
-      $button.find('.js-like-button__like-favorite').css('opacity', '1');
       this.countLike += 1;
       $button.find('.js-like-button__likes').text(this.countLike);
     } else {
-      $button.find('.js-like-button__like').css('opacity', '1');
-      $button.find('.js-like-button__like-favorite').css('opacity', '0');
       this.countLike -= 1;
       $button.find('.js-like-button__likes').text(this.countLike);
     }
