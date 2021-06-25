a235e717106f56755a28723461a2d37d0b3c93a4
5cobak <ins-arslanov@mail.ru>
2021-06-25 17:31:04 +0300

    fixed builds error :D

--- src/pages/filter/filter.js
+++ src/pages/filter/filter.js
@@ -4,7 +4,7 @@ import 'components/range-slider/range-slider.js';
 import 'components/dropdown-choose/dropdown-choose';
 import 'components/filter-date/filter-date';
 import 'components/card-room/card-room';
-import 'js/plugins/simplepagination.js';
+import 'js/plugins/simplePagination.js';
 import 'components/pagination/pagination';
 
 import './filter.scss';
